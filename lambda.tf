19d29e454452c2313ae7be2425985c0f99252904
nigelarcher <git@archerzone.com>
2019-03-19 22:57:25 +1100

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -23,5 +23,5 @@ resource "aws_lambda_permission" "apigw" {
   function_name = "${aws_lambda_function.main.arn}"
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "arn:aws:execute-api:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.proxy.id}/*/*/*"
+  source_arn = "arn:aws:execute-api:${var.aws_region}:${data.aws_caller_identity.current.account_id}:${aws_api_gateway_rest_api.proxy.id}/*/*/*"
 }
